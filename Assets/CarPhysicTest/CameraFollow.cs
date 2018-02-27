080103f76d7c91ae36f608c5ad8f9849e6963cf0
Unknown <pm4281@gmail.com>
2018-02-27 09:42:17 +0100

    Car physic test.
    Car physic fix.

---
+++ Assets/CarPhysicTest/CameraFollow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraUI { 
+
+    //CameraArm
+    //  Camera
+
+    /// <summary>
+    /// Follow target by arm of camera
+    /// </summary>
+    public class CameraFollow : MonoBehaviour {
+
+        [SerializeField] float smoothTime = 0.3F; // smoothness when interpolating
+        [SerializeField] Transform[] players; // list of players
+
+        Vector3 moveVelocity; //Veclocity of camera
+        Vector3 avaragePosition; // avarage position of targets
+
+        void FixedUpdate(){
+            Move ();
+	    }
+
+        public void Move()
+        {
+
+            //Calculate acarage position of players
+            avaragePosition = new Vector3 ();
+            for (int i = 0; i < players.Length; i++)
+                avaragePosition += players[i].position;
+
+            avaragePosition /= players.Length;
+            avaragePosition.y = 0f;
+
+            //follow this position
+            transform.position = Vector3.SmoothDamp (transform.position, avaragePosition, ref moveVelocity, smoothTime);
+        }
+
+        public Transform[] GetPlayers()
+        {
+            return players;
+        }
+        public void SetTransformTargets(List<Transform> targets)
+        {
+            players = targets.ToArray ();
+        }
+    }
+}
---
+++ Assets/CarPhysicTest/CarControler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControler : MonoBehaviour {
+
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+    Rigidbody rigibody;
+    float m_OldRotation;
+    void Start()
+    {
+        rigibody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+        Debug.Log(rigibody.velocity.magnitude);
+
+        // this if is needed to avoid gimbal lock problems that will make the car suddenly shift direction
+        if (Mathf.Abs(m_OldRotation - transform.eulerAngles.y) < 10f)
+        {
+            var turnadjust = (transform.eulerAngles.y - m_OldRotation) * 0.64f;
+            Quaternion velRotation = Quaternion.AngleAxis(turnadjust, Vector3.up);
+            rigibody.velocity = velRotation * rigibody.velocity;
+        }
+        m_OldRotation = transform.eulerAngles.y;
+
+        if (motor < 0 && rigibody.velocity.magnitude > 10) {
+            axleInfos[0].leftWheel.brakeTorque = 200000;
+            axleInfos[0].rightWheel.brakeTorque = 200000;
+            axleInfos[1].leftWheel.brakeTorque = 200000;
+            axleInfos[1].rightWheel.brakeTorque = 200000;
+        }
+        else
+        {
+            axleInfos[0].leftWheel.brakeTorque = 0;
+            axleInfos[0].rightWheel.brakeTorque = 0;
+            axleInfos[1].leftWheel.brakeTorque = 0;
+            axleInfos[1].rightWheel.brakeTorque = 0;
+        }
+    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
