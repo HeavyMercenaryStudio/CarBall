82b653de710ca8e970dc6f3079ee5eeb06acda0d
Unknown <pm4281@gmail.com>
2018-03-18 12:03:43 +0100

    Cleaning folders structure

---
+++ Assets/_MainScene/Player/CarController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+[NetworkSettings(channel = 0, sendInterval = 01f)]
+public class CarController : NetworkBehaviour {
+
+   
+    [Header("Movement")]
+    [SerializeField] float carMaxSpeed;
+    [SerializeField] float carNormalAccel;
+    [SerializeField] float carBoostedAccel;
+    [SerializeField] float rotSpeed;
+
+    [Header("Utility")]
+    [SerializeField] float kickForce;
+    [SerializeField] float maxBoostEnergy;
+    [SerializeField] ParticleSystem boostParticle;
+
+    Rigidbody rigibody;
+    float currentBoost;
+    float currentCarAccel;
+    public float CurrentBoost
+    {
+        get {
+            return currentBoost;
+        }
+        set {
+            currentBoost = Mathf.Clamp(value, 0, maxBoostEnergy);
+            if (isLocalPlayer) UI.SetEnergyBarValue(currentBoost / maxBoostEnergy);
+        }
+    }
+    public float MaxBoost
+    { get { return maxBoostEnergy; } }
+
+    CarUI UI;
+    // Use this for initialization
+    void Start () {
+        rigibody = GetComponent<Rigidbody>();
+        UI = FindObjectOfType<CarUI>();
+
+        currentBoost = maxBoostEnergy;
+        currentCarAccel = carNormalAccel;
+
+        if (!isLocalPlayer)
+            return;
+
+        UI.energyBar = GameObject.FindGameObjectWithTag("BoostPointer").GetComponent<Image>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        Move();
+    }
+   
+    private void Move()
+    {
+        //Get input
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        SpeedBoost(ref v);
+
+
+        if (v < 0) h = -h; //turn fix
+
+       //Move car
+        var force = transform.forward * currentCarAccel * v;
+        rigibody.AddForce(force);
+
+        //Lock max ca speed
+        if (rigibody.velocity.magnitude > carMaxSpeed)
+            rigibody.velocity = carMaxSpeed * rigibody.velocity.normalized; 
+
+
+        //Rotate 
+        var angle = transform.rotation.eulerAngles.y;
+        var rot = Quaternion.Euler(0, h * rotSpeed * Time.deltaTime + angle, 0);
+        rigibody.MoveRotation(rot);
+    }
+
+    private void SpeedBoost(ref float v)
+    {
+        //If boost button click
+        if (Input.GetKey(KeyCode.Space))
+        { 
+            if (CurrentBoost == 0)
+            {  // if no more boost
+                ResetCarSpeed();
+                return;
+            }
+            boostParticle.startLifetime = 1f;
+            currentCarAccel = carBoostedAccel; //incrementSpeedBy boost speed
+            CurrentBoost -= 0.25f; // boost lost per frame
+            v = 1; // drive only forward 
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+            ResetCarSpeed();
+
+    }
+
+    private void ResetCarSpeed()
+    {
+        boostParticle.startLifetime = 0.15f;
+        currentCarAccel = carNormalAccel;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (collision.gameObject.layer == Layers.BALL)
+        {
+            var ball = collision.gameObject.GetComponent<Rigidbody>();
+            var force = rigibody.velocity * kickForce;
+            if (isServer)  ball.AddForce(force);
+            
+            CmdHitBall(force, ball.gameObject);
+        }
+        
+    }
+
+    [Command]
+    void CmdHitBall(Vector3 force, GameObject ball)
+    {
+        rigibody.AddForce(force);
+    }
+}
---
+++ Assets/_MainScene/SpeedPack/SpeedPack.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SpeedPack : NetworkBehaviour {
+
+    [SerializeField] float speedPercentageAmount;
+    [SerializeField] float speedBoostRespawnTime;
+
+    bool isReady = true;
+
+    ParticleSystem particle;
+
+    [Server]
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == Layers.PLAYER)
+        {
+            AddSpeedBoost(other);
+        }
+    }
+    
+    private void AddSpeedBoost(Collider other)
+    {
+        if (!isReady) return;
+
+        var car = other.GetComponent<CarController>();
+        if (car)
+        {
+            //car.CurrentBoost += (speedPercentageAmount/car.MaxBoost) * car.MaxBoost;
+            //Debug.Log(car.GetComponent<NetworkIdentity>().netId);
+            RpcUpdateBoost(car.netId);
+            RpcStartCoroutine();
+            StartCoroutine(RespawnBoost());
+        }
+    }
+ 
+    [ClientRpc]
+    private void RpcUpdateBoost(NetworkInstanceId netId)
+    {
+        //if()
+        var list = FindObjectsOfType<CarController>();
+        for (int i = 0; i < list.Length; i++)
+        {
+            if(list[i].netId==netId)
+            {
+                list[i].CurrentBoost += (speedPercentageAmount / list[i].MaxBoost) * list[i].MaxBoost;
+                break;
+            }
+        }
+    }
+
+    [ClientRpc]
+    private void RpcStartCoroutine()
+    {
+        Debug.Log("Odpalony");
+        StartCoroutine(RespawnBoost());
+    }
+
+    private IEnumerator RespawnBoost()
+    {
+        isReady = false;
+        particle.gameObject.SetActive(false);
+        yield return new WaitForSeconds(speedBoostRespawnTime);
+        particle.gameObject.SetActive(true);
+        isReady = true;
+    }
+
+    void Start(){
+        particle = GetComponentInChildren<ParticleSystem>();
+    }
+}
